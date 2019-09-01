ce85d4976ab5f3693851df8aaeee32a8b4d53de3
Kazuo Tsubaki <kazz12211@gmail.com>
2019-09-02 01:14:13 +0900

    mateial icons

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material';
 import { MatButtonToggleModule } from '@angular/material';
+import { MatIconModule } from '@angular/material';
 
 const MaterialComponents = [
   MatButtonModule,
-  MatButtonToggleModule
+  MatButtonToggleModule,
+  MatIconModule
 ];
 
 @NgModule({
