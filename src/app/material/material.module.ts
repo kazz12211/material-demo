e4471f6f2c314324f181b5a6e0901b025ef01a90
Kazuo Tsubaki <kazz12211@gmail.com>
2019-09-02 01:33:57 +0900

    spinner

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,12 +3,14 @@ import { MatButtonModule } from '@angular/material';
 import { MatButtonToggleModule } from '@angular/material';
 import { MatIconModule } from '@angular/material';
 import { MatBadgeModule } from '@angular/material/badge';
+import { MatProgressSpinnerModule } from '@angular/material';
 
 const MaterialComponents = [
   MatButtonModule,
   MatButtonToggleModule,
   MatIconModule,
-  MatBadgeModule
+  MatBadgeModule,
+  MatProgressSpinnerModule
 ];
 
 @NgModule({
