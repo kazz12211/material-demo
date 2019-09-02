aa3b4a6f6ef9552e270843c80387c0b26aaeaaf2
Kazuo Tsubaki <kazz12211@gmail.com>
2019-09-03 00:29:49 +0900

    input

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,19 +1,26 @@
 import { NgModule } from '@angular/core';
-import { MatButtonModule } from '@angular/material';
-import { MatButtonToggleModule } from '@angular/material';
-import { MatIconModule } from '@angular/material';
+import { 
+  MatButtonModule,
+  MatButtonToggleModule, 
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatToolbarModule,
+  MatSidenavModule,
+  MatMenuModule,
+  MatListModule,
+  MatDividerModule,
+  MatGridListModule,
+  MatExpansionModule,
+  MatCardModule,
+  MatTabsModule,
+  MatStepperModule,
+  MatFormFieldModule,
+  MatInputModule
+} from '@angular/material';
+
 import { MatBadgeModule } from '@angular/material/badge';
-import { MatProgressSpinnerModule } from '@angular/material';
-import { MatToolbarModule } from '@angular/material';
-import { MatSidenavModule } from '@angular/material';
-import { MatMenuModule } from '@angular/material';
-import { MatListModule } from '@angular/material';
-import { MatDividerModule } from '@angular/material';
-import { MatGridListModule } from '@angular/material';
-import { MatExpansionModule } from '@angular/material';
-import { MatCardModule } from '@angular/material';
-import { MatTabsModule } from '@angular/material';
-import { MatStepperModule } from '@angular/material';
+
+
 const material = [
   MatButtonModule,
   MatButtonToggleModule,
@@ -29,7 +36,9 @@ const material = [
   MatExpansionModule,
   MatCardModule,
   MatTabsModule,
-  MatStepperModule
+  MatStepperModule,
+  MatFormFieldModule,
+  MatInputModule
 ];
 
 @NgModule({
